b9e9474e74b26cd628aa46d2394a6f5d3146bd93
Mirosław Opoń <opon.miroslaw@gmail.com>
2023-04-20 21:18:35 +0200

    Job and Person

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Electrician.java
@@ -0,0 +1,7 @@
+package com.kodilla.abstracts.homework;
+
+public class Electrician extends Job {
+    public Electrician(int salary, String responsibilities) {
+        super(salary, responsibilities);
+    }
+}
---
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Mechanic.java
@@ -0,0 +1,7 @@
+package com.kodilla.abstracts.homework;
+
+public class Mechanic extends Job {
+    public Mechanic(int salary, String responsibilities) {
+        super(salary, responsibilities);
+    }
+}
