6addef9aacd090c03b9974274619b53657115808
Mirosław Opoń <opon.miroslaw@gmail.com>
2023-04-25 11:42:07 +0200

    CashMachine, Bank

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/Bank.java
@@ -0,0 +1,61 @@
+package com.kodilla.bank.homework;
+
+public class Bank {
+    private CashMachine[] cashMachines;
+
+    public Bank(CashMachine[] cashMachines) {
+        this.cashMachines = cashMachines;
+    }
+
+    public double getBalance() {
+        double sum = 0;
+
+        for (CashMachine cashMachine : cashMachines) {
+            sum = sum + cashMachine.getBalance();
+        }
+
+        return sum;
+    }
+
+    public int getPayoutsCount() {
+        int payoutsCount = 0;
+
+        for (CashMachine cashMachine : cashMachines) {
+            payoutsCount = payoutsCount + cashMachine.getPayoutsCount();
+        }
+        return payoutsCount;
+    }
+
+    public int getPaymentsCount() {
+        int paymentsCount = 0;
+
+        for (CashMachine cashMachine : cashMachines) {
+            paymentsCount = paymentsCount + cashMachine.getPaymentsCount();
+        }
+        return paymentsCount;
+    }
+
+    public double getPayoutsAverage() {
+        double payoutsSum = 0;
+
+        for (CashMachine cashMachine : cashMachines) {
+            payoutsSum = payoutsSum + cashMachine.getPayoutsSum();
+        }
+        if (getPayoutsCount() == 0) {
+            return 0;
+        }
+        return payoutsSum / getPayoutsCount();
+    }
+
+    public double getPaymentsAverage() {
+        double paymentsSum = 0;
+
+        for (CashMachine cashMachine : cashMachines) {
+            paymentsSum = paymentsSum + cashMachine.getPaymentsSum();
+        }
+        if (getPaymentsCount() == 0) {
+            return 0;
+        }
+        return paymentsSum / getPaymentsCount();
+    }
+}
---
+++ kodilla-basic-tests/src/main/java/com/kodilla/bank/homework/CashMachine.java
@@ -0,0 +1,75 @@
+package com.kodilla.bank.homework;
+
+    public class CashMachine {
+        private double[] transactions;
+
+        public CashMachine(double[] transactions) {
+            this.transactions = transactions;
+        }
+
+        public double getBalance() {
+            double sum = 0;
+
+            for (double transaction : transactions) {
+                sum = sum + transaction;
+            }
+
+            return sum;
+        }
+
+        public int getTransactionsCount() {
+            return transactions.length;
+        }
+
+        public int getPayoutsCount() {
+            int count = 0;
+
+            for (double transaction : transactions) {
+                if (transaction < 0) {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public int getPaymentsCount() {
+            int count = 0;
+
+            for (double transaction : transactions) {
+                if (transaction > 0) {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public double getPayoutsSum() {
+            double sum = 0;
+
+            for (double transaction : transactions) {
+                if (transaction < 0) {
+                    sum = sum - transaction;
+                }
+            }
+
+            return sum;
+        }
+
+        public double getPaymentsSum() {
+            double sum = 0;
+
+            for (double transaction : transactions) {
+                if (transaction > 0) {
+                    sum = sum + transaction;
+                }
+            }
+
+            return sum;
+        }
+    }
+
+
+
+
+
