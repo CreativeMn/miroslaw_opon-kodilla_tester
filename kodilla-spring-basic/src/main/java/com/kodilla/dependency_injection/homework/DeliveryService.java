3bf4e12379698a27e4b2bb885f207ca5eda040fd
Mirosław Opoń <opon.miroslaw@gmail.com>
2023-05-31 21:31:44 +0200

    9.1 Spring wstep

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/dependency_injection/homework/DeliveryService.java
@@ -0,0 +1,13 @@
+package com.kodilla.dependency_injection.homework;
+
+public class DeliveryService implements DeliveryServiceInterface {
+    @Override
+    public boolean deliverPackage(String address, double weight) {
+        if (weight > 30) {
+            System.out.println("Package too heavy");
+            return false;
+        }
+        System.out.println("Delivering in progress...");
+        return true;
+    }
+}
---
+++ kodilla-spring-basic/src/main/java/com/kodilla/dependency_injection/homework/NotificationService.java
@@ -0,0 +1,12 @@
+package com.kodilla.dependency_injection.homework;
+
+public class NotificationService implements NotificationServiceInterface {
+    @Override
+    public void success(String address) {
+        System.out.println("Package delivered to: " + address);
+    }
+    @Override
+    public void fail(String address) {
+        System.out.println("Package not delivered to: " + address);
+    }
+}
